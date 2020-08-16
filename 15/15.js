85255ec364ecf7d7ef96204dc65f39e58a6bdfd6
Navraj808620 <66042799+Navraj808620@users.noreply.github.com>
2020-08-15 17:23:30 -0700

    re

--- 15/15.js
+++ 15/15.js
@@ -1,66 +1,73 @@
-add =  function() {
-result = parseInt(jQuery( '#op1' ).val()) + parseInt(jQuery( '#op2' ).val());
+var first=0;
+var second=0;
 
-text= jQuery( '#op1' ).val() +  " + "   + jQuery( '#op2 ' ).val() + "  =  ";
-result = text + result;
-jQuery ( '#result' ).html (result);
-
-old_div_content = jQuery('#history').html();
-result ="<span id='addstyle'>" + result + "</span>";
-new_div_content = old_div_content + result + '<br>';
-jQuery('#history').html(new_div_content);
+add = function() 
+{ 
+    first = $('#operand_1').val();
+    second = $('#operand_2').val();    
+    result= parseInt(first)+parseInt(second);
+    if(first & second){
+        jQuery('#result').html(first+"+"+second+ "=" + (result));
+        jQuery('#history').append("<br>"+first+"+"+second+ "=" + (result));
+    }
+    
+    
 }
-sub =  function() {
-result = parseInt(jQuery( '#op1' ).val()) - parseInt(jQuery( '#op2' ).val());
+sub = function() 
+{   first = $('#operand_1').val();
+    second = $('#operand_2').val();    
+    result= parseInt(first)-parseInt(second);
 
-text= jQuery( '#op1' ).val() +  " - "   + jQuery( '#op2 ' ).val() + "  =  ";
-result = text + result;
-jQuery ( '#result' ).html  (result);
-
-result = "<span id='substyle'>" + result + "</span>";
-old_div_content = jQuery('#history').html();
-new_div_content = old_div_content + result + '<br>';
-jQuery('#history').html(new_div_content);
+    if(first & second){
+    jQuery('#result').html(first+"-"+second+ "=" + (result));
+    jQuery('#history').append("<br>"+first+"-"+second+ "=" + (result)+ "\n");
+    }
 }
-mul =  function() {
-result = parseInt(jQuery( '#op1' ).val()) * parseInt(jQuery( '#op2' ).val());
+mul = function() 
+{   first = $('#operand_1').val();
+    second = $('#operand_2').val();    
+    result= parseInt(first)*parseInt(second);
 
-text= jQuery( '#op1' ).val() +  " * "   + jQuery( '#op2 ' ).val() + "  =  ";
-result = text + result;
-jQuery ( '#result' ).html (result);
-
-result ="<span id='mulstyle'>" + result + "</span>";
-old_div_content = jQuery('#history').html();
-new_div_content = old_div_content + result + '<br>';
-jQuery('#history').html(new_div_content);
+    if(first & second){
+    jQuery('#result').html(first+"*"+second+ "=" + (result));
+    jQuery('#history').append("<br>"+first+"*"+second+ "=" + (result)+ "\n");
+    }
 }
-div =  function() {
-result = parseInt(jQuery( '#op1' ).val()) / parseInt(jQuery( '#op2' ).val());
-
-text= jQuery( '#op1' ).val() +  " / "   + jQuery( '#op2 ' ).val() + "  =  ";
-result = text + result;
-jQuery ( '#result' ).html (result);
-
-result ="<span id='divstyle'>" + result + "</span>";
-old_div_content = jQuery('#history').html();
-new_div_content = old_div_content + result + '<br>';
-jQuery('#history').html(new_div_content);
+div = function() 
+{   first = $('#operand_1').val();
+    second = $('#operand_2').val();    
+    result= parseInt(first)/parseInt(second);
+    if(first & second){
+    jQuery('#result').html(first+"/"+second+ "=" + (result));
+    jQuery('#history').append("<br>"+first+"/"+second+ "=" + (result)+ "\n");
+    }
 }
-
-show = function(){
+clear=function()
+{
+    result=" ";
+    jQuery('#result').html(result);
+    jQuery('#history').html(result);
+}
+show = function()
+{
 jQuery('#history').show();
 }
 
-hide = function(){
+hide = function()
+{
 jQuery('#history').hide();
 }
 
-setup= function() {
-jQuery( '#addbutton' ).click(add);
-jQuery( '#subbutton' ).click(sub);
-jQuery( '#mulbutton' ).click(mul);
-jQuery( '#divbutton' ).click(div);
-jQuery( '#show_button' ).click(show);
-jQuery( '#hide_button' ).click(hide);
+setup = function() {    
+    jQuery('#add_button').click(add)
+    jQuery('#add_button').mouseover(add)
+    jQuery('#sub_button').click(sub)
+    jQuery('#sub_button').mouseover(sub)
+    jQuery('#mul_button').click(mul)
+    jQuery('#mul_button').mouseover(mul)
+    jQuery('#div_button').click(div)
+    jQuery('#div_button').mouseover(div)
+	jQuery('#show_button').click(show)
+	jQuery('#hide_button').click(hide)
 }
-jQuery(document).ready(setup);
+jQuery(document).ready(setup)
